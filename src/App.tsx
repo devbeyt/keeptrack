6fec1faa47e39767d6ec1bedd7112283876c4cb8
= <=>
2022-09-01 20:54:44 +0400

    fist commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function App() {
+  return (<blockquote cite="Benjamin Franklin">
+  +      Tell me and I forget, teach me and I may remember, involve me and I learn.
+  +    </blockquote>
+  )
+}
+
+export default App
