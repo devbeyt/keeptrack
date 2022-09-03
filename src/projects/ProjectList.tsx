2b8d774af754d1f38fbedb268530d516bdf751ce
= <=>
2022-09-03 17:54:56 +0400

    completed labs 10

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,39 +1,48 @@
-import React from 'react'
 import { Project } from './Project';
+import ProjectCard from './ProjectCard'
+import ProjectForm from './ProjectForm'
 
-interface ProjectListProps{
+interface ProjectListProps {
     projects: Project[];
 }
 
-function ProjectList({projects}:ProjectListProps) {
-//    return <pre>{JSON.stringify(projects,null,'')}</pre>
+function ProjectList({ projects }: ProjectListProps) {
+    //    return <pre>{JSON.stringify(projects,null,'')}</pre>
 
-//  return (<ul className='row'>
-//       {
-//         projects.map(project=>(
-//             <li key={project.id}>{project.name}</li>
-//         ))
-//       }
-//  </ul>)
+    //  return (<ul className='row'>
+    //       {
+    //         projects.map(project=>(
+    //             <li key={project.id}>{project.name}</li>
+    //         ))
+    //       }
+    //  </ul>)
 
-return (
-    <div className="row">
-         {projects.map((project) => (
-           <div key={project.id} className="cols-sm">
-             <div className="card">
-               <img src={project.imageUrl} alt={project.name} />
-               <section className="section dark">
-                 <h5 className="strong">
-                   <strong>{project.name}</strong>
-                 </h5>
-                 <p>{project.description}</p>
-                 <p>Budget : {project.budget.toLocaleString()}</p>
-               </section>
-             </div>
-           </div>
-         ))}
-       </div>
-)
+    const content = projects.map(project => {
+        return <div key={project.id} className="cols-sm">
+            <ProjectCard project={project}></ProjectCard>
+            <ProjectForm />
+        </div>
+    })
+
+    return (
+        <div className="row">
+            {
+            content
+                //  <div key={project.id} className="cols-sm">
+                //    <div className="card">
+                //      <img src={project.imageUrl} alt={project.name} />
+                //      <section className="section dark">
+                //        <h5 className="strong">
+                //          <strong>{project.name}</strong>
+                //        </h5>
+                //        <p>{project.description}</p>
+                //        <p>Budget : {project.budget.toLocaleString()}</p>
+                //      </section>
+                //    </div>
+                //  </div>
+            }
+        </div>
+    )
 
 
 }
